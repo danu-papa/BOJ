66ec5c8cc9df8be9b9043a0067acb588beaba6ad
NICEMANEUL <cndtjq22@gmail.com>
2020-08-04 16:04:22 +0900

    백준 10828 - Stack API 활용해보기

--- 00_BOJ_Test/src/Set01_Basic_Data_Structure/Stack_API.java
+++ 00_BOJ_Test/src/Set01_Basic_Data_Structure/Stack_API.java
@@ -13,7 +13,7 @@ import java.util.Stack;
  * POP : 삭제
  * SIZE : 스택에 들어있는 정수 개수
  * EMPTY : 비었으면 1, 아니면 0
- * TOP : 스택 가장 위의 수를 출력. 없으면 -1
+ * TOP : 스택 가장 위의 수를 출력. 없으면 -1 
  */
 public class Stack_API {
 	public static void main(String[] args) {
