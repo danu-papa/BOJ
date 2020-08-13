129c452971ec8187c175bd493b9d5e3a6a7f14ee
NICEMANEUL <cndtjq22@gmail.com>
2020-08-13 14:18:13 +0900

    백준 6603 - 로또 프로그램

---
+++ 00_BOJ_Test/src/Set02_Back_Tracking/BOJ_6603_Lotto.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Set02_Back_Tracking;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 6603 로또
+ * 독일로또 1~49 사이의 숫자중 6개를 고름.
+ * 1~49 수중 6개 이상의 숫자를 골라 부분집합을 만들고
+ * 나오는 모든 경우의 수 중 하나를 고르는 방법이 있다.
+ * 나올 수 있는 모든 경우의 수를 출력하기.
+ * 조합 쓰면 되겠다.
+ */
+public class BOJ_6603_Lotto {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stt = new StringTokenizer(br.readLine());
+		
+		
+	}
+}
