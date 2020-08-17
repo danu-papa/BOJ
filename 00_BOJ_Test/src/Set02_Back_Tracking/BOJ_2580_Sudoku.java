97402435342d34f44b8f23aa831be8befe535392
NICEMANEUL <cndtjq22@gmail.com>
2020-08-17 14:54:34 +0900

    백준 2580 - DFS , Backtracking을 이용해서 해결

--- 00_BOJ_Test/src/Set02_Back_Tracking/BOJ_2580_Sudoku.java
+++ 00_BOJ_Test/src/Set02_Back_Tracking/BOJ_2580_Sudoku.java
@@ -20,10 +20,8 @@ import java.util.StringTokenizer;
  * DFS수행, BackTracking
  */
 public class BOJ_2580_Sudoku {
-	static int[][] sudoku;
-	static boolean numbers[];
-
-	static List<ZeroState> zlist = new ArrayList<>();
+	static int[][] sudoku; // 스도쿠 정보를 입력할 2차원 배열
+	static List<ZeroState> zlist = new ArrayList<>(); // 스도쿠의 정보를 입력 받을 때 빈칸의 정보를 담을 리스트
 
 	// 스도쿠 판에서 비어있는 칸의 위치 정보를 저장하는 클래스
 	static class ZeroState{
@@ -35,6 +33,7 @@ public class BOJ_2580_Sudoku {
 	}
 
 	public static void main(String[] args) throws Exception {
+		// 속도를 높히기 위해서 BufferedReader와 StringTokenizer 사용
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer stt;
 
@@ -51,13 +50,15 @@ public class BOJ_2580_Sudoku {
 			}
 		} // 주어진 스도쿠의 값을 입력
 
-		solve_sudoku(0);
+		solve_sudoku(0); // 스도쿠 채우기
 	}
 
-	/** 스도쿠를 해결하자*/
+	/** 스도쿠를 해결하자!!
+	 * idx는 빈칸의 위치 정보를 찾기 위한 값. ( 몇번째 빈칸인가? 생각하는 것이 편할 듯하다.)
+	 * */
 	private static void solve_sudoku(int idx) {
-		// 빈 칸이 더이상 없는 경우
-		if( idx == zlist.size()) {
+		// 모든 빈 칸에 대해서 처리를 다 한 경우
+		if( idx == zlist.size()) { // DFS의 탈출 조건  --> 모든 빈칸을 수행한 경우
 			for(int i = 0; i <9; i++) {
 				for( int j = 0; j < 9; j++) {
 					// 모든 경우가 다 차있으므로 출력
@@ -73,11 +74,12 @@ public class BOJ_2580_Sudoku {
 		for( int i = 1; i < 10; i++) {
 			// 행, 열, 3x3
 			if(checkRow(i,idx) && checkColumns(i,idx) && checkOwnSmallBox(i,idx)) {
-				// 3 조건을 봤는데 중복되는 숫자가 하나도 없는 경우
+				// 3 조건을 봤는데 중복되는 숫자가 하나도 없는 경우. 해당 빈칸에 현재 i값을 넣고 다음 빈칸으로 넘긴다.
 				sudoku[zlist.get(idx).y][zlist.get(idx).x] = i;
 				// 다음 빈칸으로 이동
 				solve_sudoku(idx+1);
-				// 해당 빈칸에 쓰여진 숫자를 지우고 다시 빈칸으로 되돌린다.
+				// 해당 빈칸에 쓰여진 숫자를 지우고 다시 빈칸으로 되돌린다. 이 코드가 없으면 이전으로 돌아가도 스도쿠 판에는 숫자가 존재한다.
+				// 즉, 중복 숫자가 존재 할 수 있게 된다.
 				sudoku[zlist.get(idx).y][zlist.get(idx).x] = 0;
 			}
 		}
@@ -99,7 +101,8 @@ public class BOJ_2580_Sudoku {
 		return true;
 	}
 
-	/** 열 검사*/
+	/** 열 검사
+	 * y축 고정, x축만 이동*/
 	private static boolean checkColumns(int cur_num, int idx) {
 		int cur_x = zlist.get(idx).x;
 
@@ -113,7 +116,8 @@ public class BOJ_2580_Sudoku {
 		return true;
 	}
 
-	/** 행 검사*/
+	/** 행 검사
+	 * x축 고정, y축만 이동*/
 	private static boolean checkRow(int cur_num, int idx) {
 		int cur_y = zlist.get(idx).y;
 
