77214ec9060593765173255401d7ca221e6182d1
NICEMANEUL <cndtjq22@gmail.com>
2020-10-23 18:05:42 +0900

    BOJ 1647 배열로 구현. 메모리 뻥

---
+++ 00_BOJ_Test/src/BOJ_Practice/BOJ_1647_Divde_City_Plan.java
@@ -0,0 +1,220 @@
+/**
+ * 
+ */
+package BOJ_Practice;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 1647 - 도시 분할 계획 한 마을을 2개의 구역으로 나눈다. N개의 집과 M개의 길이 있다. 길은 양방향. 각 분리된 마을은 마을
+ * 안에서 집들이 서로 연결이 되어있어야 한다. 필요없는 길을 모두 없애고 남은 최소의 길의 유지비를 구하자.
+ * 정점10만개. 길 100만개..
+ */
+public class BOJ_1647_Divde_City_Plan {
+	static class Node implements Comparable<Node>{
+		int no, weight;
+
+		public Node(int no, int weight) {
+			this.no = no;
+			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.weight, o.weight);
+		}
+	}
+	
+	// N: 집의 개수 , M: 길의 개수
+	private static int N, M, map[][], resMin, tmpres;
+	private static boolean isSelected[];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stt = new StringTokenizer(br.readLine(), " ");
+
+		N = Integer.parseInt(stt.nextToken());
+		M = Integer.parseInt(stt.nextToken());
+		map = new int[N + 1][N + 1];
+		isSelected = new boolean[N + 1];
+
+		for (int i = 0; i < M; i++) {
+			stt = new StringTokenizer(br.readLine());
+			int first = Integer.parseInt(stt.nextToken());
+			int sec = Integer.parseInt(stt.nextToken());
+			int weight = Integer.parseInt(stt.nextToken());
+			map[first][sec] = weight;
+			map[sec][first] = weight;
+		} // input end
+
+		resMin = Integer.MAX_VALUE;
+		divide_region(1);
+		System.out.println(resMin);
+	}
+
+	/**
+	 * 선택할 수 있는 모든 구역의 경우를 구하자. 부분집합으로 구현
+	 */
+	private static void divide_region(int idx) {
+		if (idx == N + 1) { // 모든 경우 확인.
+			int cnt = 0;
+			for (int i = 1; i <= N; i++) {
+				if (isSelected[i])
+					cnt++;
+			}
+			// 모두 선택했거나, 하나도 선택 안했거나.
+			if (cnt == N || cnt == 0)
+				return;
+
+			if (checkConnect()) {
+				int tmpmap[][] = new int[N+1][N+1];
+				for (int i = 1; i <= N; i++) {
+					for (int j = 1; j <= N; j++) {
+						tmpmap[i][j] = map[i][j];
+					}
+				}
+				// 모두 연결이 되어있는 경우.
+				// 분리된 두 구역간 길을 모두 끊는다.
+				removeRoad(tmpmap);
+				
+				// 남은 길을 가지고 프림.
+				// 선택한 구역과, 선택하지 않은 구역을 나누어서 합산
+				tmpres = 0;
+				prim(tmpmap, true);
+				prim(tmpmap, false);
+				
+				resMin = Math.min(resMin, tmpres);
+			}
+
+			return;
+		}
+		isSelected[idx] = true;
+		divide_region(idx + 1);
+		isSelected[idx] = false;
+		divide_region(idx + 1);
+	}
+
+	/** 프림알고리즘을 이용해서 연결된 마을간 MST를 찾자 */
+	private static void prim(int[][] tmpmap, boolean flag) {
+		PriorityQueue<Node> pQueue = new PriorityQueue<Node>();
+		int[] minEdge = new int[N+1];
+		Arrays.fill(minEdge, Integer.MAX_VALUE);
+		boolean visited[] = new boolean[N+1];
+		
+		int start_vertex = 0;
+		
+		for(int i = 1; i <= N; i++) {
+			if(isSelected[i] == flag) {
+				start_vertex = i;
+				break;
+			}
+		}
+		
+		int result = 0;
+		minEdge[start_vertex] = 0;
+		pQueue.offer(new Node(start_vertex,0));
+		
+		while(!pQueue.isEmpty()) {
+			Node minVertex = pQueue.poll();
+			int cur_Vertex = minVertex.no;
+			
+			if(visited[cur_Vertex]) continue;
+			
+			result += minVertex.weight;
+			visited[cur_Vertex] = true;
+			
+			for(int j = 1; j <= N; j++) {
+				if(!visited[j] && tmpmap[cur_Vertex][j] != 0 
+						&& tmpmap[cur_Vertex][j] < minEdge[j]) {
+					pQueue.offer(new Node(j,tmpmap[cur_Vertex][j]));
+				}
+			}
+		}
+		
+		tmpres += result;
+	}
+
+	/** 두 구역간 길을 끊자 */
+	private static void removeRoad(int[][] tmpmap) {
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				// 선택한 정점에서 선택안한 정점중에 길이 있다(0이 아니다)
+				if(isSelected[i] && !isSelected[j] && tmpmap[i][j] != 0) {
+					tmpmap[i][j] = 0;
+					tmpmap[j][i] = 0;
+				}
+			}
+		}
+	}
+
+	/**
+	 * 선택된 구역이 모두 연결되었는지 확인 BFS이용
+	 */
+	private static boolean checkConnect() {
+		// 모든 곳이 연결이 되어있나 확인할 배열
+		boolean arr[] = new boolean[N + 1];
+
+		Queue<Integer> queue = new LinkedList<>();
+
+		for (int i = 1; i <= N; i++) {
+			if (isSelected[i]) { // 선택한 지점부터 탐색 시작.
+				queue.offer(i);
+				arr[i] = true;
+				break;
+			}
+		}
+
+		while (!queue.isEmpty()) {
+			int cur = queue.poll();
+
+			if (isSelected[cur]) { // 현재 정점 선택함.
+				for (int i = 1; i <= N; i++) {
+					if (arr[i]) continue;
+					if (isSelected[i]) {
+						if(map[cur][i] != 0) {
+							queue.offer(i);
+							arr[i] = true;
+						}
+					}
+				}
+			}
+		} // 선택 while end
+		
+
+		for (int i = 1; i <= N; i++) {
+			if (!isSelected[i]) { // 선택안한 지점부터 탐색 시작.
+				queue.offer(i);
+				arr[i] = true;
+				break;
+			}
+		}
+		
+		while (!queue.isEmpty()) {
+			int cur = queue.poll();
+
+			if (!isSelected[cur]) { // 현재 정점 선택함.
+				for (int i = 1; i <= N; i++) {
+					if (arr[i]) continue;
+					if (!isSelected[i]) {
+						if(map[cur][i] != 0) {
+							queue.offer(i);
+							arr[i] = true;
+						}
+					}
+				}
+			}
+		} // 선택 안한 while end
+		
+		for(int i = 1; i <= N; i++) {
+			if(!arr[i]) return false;
+		}
+		
+		return true;
+	}
+}
