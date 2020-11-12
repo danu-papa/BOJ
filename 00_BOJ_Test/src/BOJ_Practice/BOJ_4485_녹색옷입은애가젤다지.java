e48c312454f561dfa8df53ea1e4e138920de8fac
NICEMANEUL <cndtjq22@gmail.com>
2020-11-12 18:32:48 +0900

    BOJ_4485_BFS

---
+++ 00_BOJ_Test/src/BOJ_Practice/BOJ_4485_녹색옷입은애가젤다지.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package BOJ_Practice;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class BOJ_4485_녹색옷입은애가젤다지 {
+
+	static class Cell implements Comparable<Cell>{
+		int r;
+		int c;
+		int cost;
+		public Cell() {}
+		public Cell(int r, int c, int cost) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.cost = cost;
+		}
+		@Override
+		public int compareTo(Cell outer) {  //증가순 정렬
+			return Integer.compare(this.cost, outer.cost);
+		}
+
+	}
+	static int N;
+	static int[][] map;
+	static int[][] cost;
+	static int[] dr={-1,1,0,0};
+	static int[] dc={0,0,-1,1};
+
+	public static void main(String[] args) {
+		Scanner scann=new Scanner(System.in);
+		int cnt=1;
+		while(true){
+			N=scann.nextInt();
+			if(N==0){break ;}
+			map=new int[N][N];
+			cost=new int[N][N];
+			for (int i = 0; i < N; i++) {
+				Arrays.fill(cost[i], Integer.MAX_VALUE/100);
+			}
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					map[i][j]=scann.nextInt();
+				}
+			}
+			int result=bfs();
+			for (int i = 0; i < N; i++) {
+				System.out.println(Arrays.toString(cost[i]));
+			}
+			System.out.println("Problem "+(cnt++)+": "+result);
+		}
+
+	}
+
+	static int bfs() {
+		PriorityQueue<Cell> que= new PriorityQueue<>();
+		que.offer(new Cell(0,0,map[0][0]));
+		cost[0][0]=map[0][0];
+		while(!que.isEmpty()){
+			Cell cur=que.poll();
+			int cr=cur.r;
+			int cc=cur.c;
+			//if(cr==N-1 && cc==N-1){ break; }// 최단
+			// 있으나 마나
+			/*            if(cost[cr][cc]<cur.cost){
+                continue;
+            }*/
+			for (int d = 0; d < 4; d++) {
+				int nr=cr+dr[d];
+				int nc=cc+dc[d];
+				if(!check(nr,nc))continue;
+				if(cost[nr][nc]>cost[cr][cc]+map[nr][nc]){
+					//작은값으로 갱신
+					cost[nr][nc]=cost[cr][cc]+map[nr][nc];
+					que.offer(new Cell(nr,nc,map[nr][nc]));
+				}
+
+			}
+		}
+		return cost[N-1][N-1];
+	}
+
+	static boolean check(int nr, int nc) {
+		return nr>=0 && nr<N && nc>=0 && nc<N;
+	}
+}
