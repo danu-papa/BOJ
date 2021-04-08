f2397a3b225930a69229840084b2037de05b5508
NICEMANEUL <cndtjq22@gmail.com>
2021-04-08 21:06:03 +0900

    백준 2473 세용액 투포인터

--- 00_BOJ_Test/src/Two_Pointer/세용액.java
+++ 00_BOJ_Test/src/Two_Pointer/세용액.java
@@ -33,15 +33,19 @@ public class 세용액 {
 		
 		long diff = Long.MAX_VALUE;
 
+		// 가장 왼쪽부터 시작해서 가장 오른쪽 끝까지
 		for(int i = 0; i < N; i++) {
-			int left = i+1;
+			int left = i+1; // 가장 왼쪽을 제외하고 그 다음부터 순회
 			int right = N-1;
 			
 			while(left < right) {
+				// 세 용액의 합 계산
 				long sum = solutions[i] + solutions[left] + solutions[right];
 				
+				// 차이 계산
 				long cur_diff = Math.abs(sum);
 				
+				// 차이가 더 작다면 원소 저장
 				if(cur_diff < diff) {
 					diff = cur_diff;
 					res[0] = solutions[i];
@@ -49,9 +53,9 @@ public class 세용액 {
 					res[2] = solutions[right];
 				}
 				
-				if(sum > 0) {
+				if(sum > 0) { // 차이가 0보다 크다. 오른쪽 원소를 한 칸 앞으로
 					right--;
-				} else {
+				} else { // 차이가 0보다 작다. 왼쪽 원소를 한 칸 앞으로
 					left++;
 				}
 			}
